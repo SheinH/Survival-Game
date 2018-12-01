fc2e8344931d3adc0080f9e24cee729ac2c30659
Andy <andyyeung122@mail.com>
2018-12-01 09:57:21 -0500

    Update

---
+++ src/SurvivalGame/GameLogic/Tile.java
@@ -0,0 +1,12 @@
+package SurvivalGame.GameLogic;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tile {
+    class Field{
+        List<String> objects = new ArrayList<>();
+        char Tile[][];
+
+    }
+
+}

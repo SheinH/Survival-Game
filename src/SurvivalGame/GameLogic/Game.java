44a525af4851dc4d33a975f66cba009acaa1b471
SheinH <chocolateshein.htike@gmail.com>
2018-11-30 23:25:51 -0500

    Made basic game tick logic

---
+++ src/SurvivalGame/GameLogic/Game.java
@@ -0,0 +1,40 @@
+package SurvivalGame.GameLogic;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Game {
+    Field field;
+    ScheduledExecutorService gameThread;
+    ReentrantLock gameLock;
+    boolean paused;
+    ReentrantLock gamePauseLock;
+    public Game(){
+        gamelock = new ReentrantLock();
+        gamePauseLock = new ReentrantLock();
+        gameThread = Executors.newSingleThreadScheduledExecutor();
+        gameThread.scheduleAtFixedRate(() -> update(), 0, 33333, TimeUnit.MILLISECONDS);
+    }
+
+    public void update(){
+        gameLock.lock();
+        gamePauseLock.lock();
+        field.getFieldObjects().forEach((obj) -> obj.update());
+        gameLock.unlock();
+    }
+
+    public void pause(){
+        if(!paused){
+            gamePauseLock.lock();
+            paused = true;
+        }
+    }
+
+    public void unPause(){
+        if(paused){
+            gamePauseLock.unlock();
+            paused = false;
+        }
+    }
+}

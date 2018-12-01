caad59b5e77ea5e05193899a730a422d1249978b
SheinH <chocolateshein.htike@gmail.com>
2018-12-01 12:53:36 -0500

    Added pausing to game, added hashmap to MovingFieldObject

--- src/SurvivalGame/GameLogic/FieldObjects/MovingFieldObject.java
+++ src/SurvivalGame/GameLogic/FieldObjects/MovingFieldObject.java
@@ -1,10 +1,10 @@
 package SurvivalGame.GameLogic.FieldObjects;
-
 import SurvivalGame.GameLogic.Terrain;
+import java.util.HashMap;
 
 public abstract class MovingFieldObject extends FieldObject {
     private int direction;
-    private int[] moveSpeeds;
+    private HashMap<Terrain,Integer> moveSpeeds;
     public abstract int getMoveTime();
     private static Terrain[] moveableTerrains;
     public int getDirection(){

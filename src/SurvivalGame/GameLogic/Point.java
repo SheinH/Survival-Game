bd0f8e1e0745ca0fc74bd2d0860fd46219fd5ec4
techscience67 <computerclasses77@gmail.com>
2018-12-01 22:29:41 -0500

    Point.java created & update UML

---
+++ src/SurvivalGame/GameLogic/Point.java
@@ -0,0 +1,51 @@
+package SurvivalGame.GameLogic;
+
+public class Point{
+    private int x;
+    private int y;
+
+    //#CONSTRUCTORS
+    public Point(int x){
+        this(x, 0);
+    }
+
+    public Point(){
+        this(0,0);
+    }
+
+    public Point(int x, int y){
+        if (x < 0){
+            throw new IllegalArgumentException("x must be >=0");
+        }
+
+        if(y < 0){
+            throw new IllegalArgumentException("y must be >=0");
+        }
+
+        this.x  = x;
+        this.y  = y;
+    }
+
+    //#getter methods
+
+    public int getX(){return x;}
+
+    public int getY(){return y;}
+
+    //#setter methods
+
+    public void setX(){
+        if (x < 0){
+            throw new IllegalArgumentException("x must be >=0");
+        }
+        this.x = x;}
+
+    public void setY(){
+        if(y < 0){
+            throw new IllegalArgumentException("y must be >=0");
+        }
+
+        this.y = y;
+    }
+
+}

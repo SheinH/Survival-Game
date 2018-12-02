19e5df56e75553048de5c429efe5e3c665825ce8
SheinH <chocolateshein.htike@gmail.com>
2018-12-02 17:22:11 -0500

    Collectable, Bush, Tree, and Tile tweaked

---
+++ src/SurvivalGame/GameLogic/FieldObjects/Bush.java
@@ -0,0 +1,15 @@
+package SurvivalGame.GameLogic.FieldObjects;
+
+
+public class Bush extends FieldObject implements Collectable {
+    int total = 10;
+    int giveitems(int items){
+        return items;
+    }
+    public void addItem() {
+        for (int i = 0; i < total; i++) {
+            items.add("Berries");
+        }
+    }
+}
+
---
+++ src/SurvivalGame/GameLogic/FieldObjects/Tree.java
@@ -0,0 +1,12 @@
+package SurvivalGame.GameLogic.FieldObjects;
+
+public class Tree extends FieldObject implements Collectable {
+    int total = 20;
+    int giveitems(int items){
+        return items;
+    }
+    public void addites(){
+        for (int i = 0; i < total; i++){
+            items.add("Sticks");
+        }
+    }

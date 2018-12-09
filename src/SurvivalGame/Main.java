8c77590f42601bb9ed1e3c3663e1cd6cc3ea928e
Andy <andyyeung122@mail.com>
2018-12-09 17:31:01 -0500

    Readded platform

--- src/SurvivalGame/Main.java
+++ src/SurvivalGame/Main.java
@@ -3,19 +3,33 @@ package SurvivalGame;
 import SurvivalGame.GameLogic.SurvivalGame;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.Button;
 
 public class Main extends Application {
 
     private Controller c;
     private SurvivalGame game;
+    Button button;
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
         c = new Controller();
+
+        Button btn = new Button();
+        button.setLabel("Quit");
+
+        StackPane sp = new StackPane();
+        sp.getChildren().add(btn);
+
         loader.setController(c);
         Parent root = loader.load();
         primaryStage.setTitle("Survival Game");

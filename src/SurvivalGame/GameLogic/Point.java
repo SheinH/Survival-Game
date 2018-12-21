7f113f1cd54116b24ca9c24ccf490f32578ab48e
techscience67 <computerclasses77@gmail.com>
2018-12-21 01:18:48 -0500

    Merge branch 'master' of https://github.com/SheinH/Survival-Game
    # Conflicts:
    #	src/SurvivalGame/GameLogic/FieldObjects/Carnivore.java

--- src/SurvivalGame/GameLogic/Point.java
+++ src/SurvivalGame/GameLogic/Point.java
@@ -14,13 +14,6 @@ public class Point{
     }
 
     public Point(int y, int x){
-        if(x < 0) {
-            throw new IllegalArgumentException("x must be >= 0");
-        }
-
-        if (y < 0) {
-            throw new IllegalArgumentException("y must be >= 0");
-        }
 
         this.x  = x;
         this.y  = y;
